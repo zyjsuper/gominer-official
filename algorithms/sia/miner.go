07a5facd8034b4720b427fe2e1ccd3d488e89c36
zyjsuper <zyj_super2@163.com>
2018-06-25 14:28:47 +0800

    Update miner.go

--- algorithms/sia/miner.go
+++ algorithms/sia/miner.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/bitcoinx-project/gominer/clients"
 	"github.com/bitcoinx-project/gominer/mining"
-	"github.com/zyjsuper/go-opencl/cl"
+	"github.com/robvanmieghem/go-opencl/cl"
 )
 
 //miningWork is sent to the mining routines and defines what ranges should be searched for a matching nonce
